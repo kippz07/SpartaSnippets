4a892f012f9c993432f22c88fd247b13e48eb428
Jenny Johnson <kippz07@users.noreply.github.com>
2017-08-29 19:34:42 +0100

    added restful routes

--- app/controllers/snippet_controller.rb
+++ app/controllers/snippet_controller.rb
@@ -4,4 +4,32 @@ class SnippetController < ApplicationController
         @snippets = Snippet.all
     end
 
+    def new
+        @snippet = Snippet.new
+    end
+
+    def create
+        @snippet = Snippet.create(snippet_params)
+        @snippet.save
+        redirect_to "/"
+    end
+
+    def edit
+    end
+
+    def update
+        @snippet.update(snippet_params)
+        redirect_to "/"
+    end
+
+    def destroy
+        @snippet.destroy
+        redirect_to "/"
+    end
+
+    private
+        def snippet_params
+            params.require(:snippet).permit(:question, :answer)
+        end
+
 end
