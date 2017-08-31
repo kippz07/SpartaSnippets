b7c69c411900b614629530098a06418aef90fc1c
Jenny Johnson <kippz07@users.noreply.github.com>
2017-08-31 14:52:41 +0100

    added play button

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -1,5 +1,15 @@
 $(function() {
 
+    function launchFullScreen(element) {
+      if(element.requestFullScreen) {
+        element.requestFullScreen();
+      } else if(element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if(element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
+      }
+    }
+
     setInterval(function () {
         $.get("http://localhost:3000/pi.json", function (data) {
             $("#q").text(data.question)
@@ -7,5 +17,11 @@ $(function() {
             console.log(data);
         });
     }, 5000);
+
+    $("#play-link").click(function (element) {
+        launchFullScreen(document.documentElement);
+    })
+
+    
 
 });
