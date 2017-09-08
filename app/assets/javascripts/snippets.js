d046ccd68c4e2045dd450c2e36a89d0a76eae631
Jenny Johnson <kippz07@users.noreply.github.com>
2017-09-08 11:19:54 +0100

    snippets only selected on the pi page

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -10,13 +10,16 @@ $(function() {
       }
     }
 
-    // // setInterval(function () {
-    // //     // $.get("https://spartasnippets.herokuapp.com/pi.json", function (data) {
-    // //     $.get("pi.json", function (data) {
-    // //         $("#q").text(data.question);
-    // //         $("#a").text(data.answer);
-    // //     });
-    // // }, 3000);
+    setInterval(function () {
+        // if(window.location.href == "https://spartasnippets.herokuapp.com/pi") {
+        if(window.location.href == "http://localhost:3000/pi") {
+            // $.get("https://spartasnippets.herokuapp.com/pi.json", function (data) {
+            $.get("pi.json", function (data) {
+                $("#q").text(data.question);
+                $("#a").text(data.answer);
+            });
+        }    
+    }, 3000);
 
     $("#fullscreen-button").click(function (element) {
         launchFullScreen(document.documentElement);
