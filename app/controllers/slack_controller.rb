57644ba11eacdee697027ac61e8f2e694b1bff29
Jenny Johnson <kippz07@users.noreply.github.com>
2017-09-09 15:35:49 +0100

    added create snippet to slack post route

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,11 +1,17 @@
 class SlackController < ApplicationController
 
+
+
     def create
         # if params[:token] == hl75A8UzLIUAZAkjbEgDtB9c
         #     puts params[:text]
         # else
         #     puts params[:user_name]
         # end
+
+        @snippet = Snippet.create(params[:user_name], params[:text])
+        @snippet.save
+
         render json: { text: "Recieved" }       
     end
 
