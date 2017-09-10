d5deaf4edce7b22837a1c4795ed188ac913ae6c7
Jenny Johnson <kippz07@users.noreply.github.com>
2017-09-10 17:31:05 +0100

    added more styling to the pi page

--- app/assets/javascripts/snippets.js
+++ app/assets/javascripts/snippets.js
@@ -1,27 +1,35 @@
 $(function() {
 
-    var arrayLength = $("#array-length");
-    var length = arrayLength.html();
+    var $arrayLength = $('#array-length');
+    var length = $arrayLength.html();
+    var $image = $('.bg-img');
+    var $body = $('body');
 
     setInterval(function () {
-        if(window.location.href == "https://spartasnippets.herokuapp.com/pi") {
-        // if(window.location.href == "http://localhost:3000/pi") {
-            $.get("https://spartasnippets.herokuapp.com/pi.json", function (data) {
-            // $.get("pi.json", function (data) {
-                $("#q").text(data.question);
-                $("#a").text(data.answer);
+        // if(window.location.href == "https://spartasnippets.herokuapp.com/pi") {
+        if(window.location.href == 'http://localhost:3000/pi') {
+            // $.get("https://spartasnippets.herokuapp.com/pi.json", function (data) {
+            $.get('pi.json', function (data) {
+                $('#q').text(data.question);
+                $('#a').text(data.answer);
             });
             // console.log(length);
             // var curLength = arrayLength.html();
             // length = checkForNewSnippets(length, curLength);
             // console.log(curLength);
             // console.log(length);
-        } 
+            
+        }
     }, 3000);
 
-    $("#fullscreen-button").click(function (element) {
+    if(window.location.href == 'http://localhost:3000/pi') {
+        $body.addClass('bg-img');
+    } else {
+        $body.removeClass('bg-img');
+    }
+
+    $('#fullscreen-button').click(function (element) {
         launchFullScreen(document.documentElement);
-        
     })
 
     $(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
@@ -29,8 +37,8 @@ $(function() {
         var event = state ? 'FullscreenOn' : 'FullscreenOff';
 
         if(event == 'FullscreenOff') {
-            $("header").slideDown();
-            $("#fullscreen-button").slideDown();
+            $('header').slideDown();
+            $('#fullscreen-button').slideDown();
         }  
     });
 
@@ -51,8 +59,12 @@ $(function() {
         } else if(element.webkitRequestFullScreen) {
         element.webkitRequestFullScreen();
         }
-        $("header").slideUp();
-        $("#fullscreen-button").slideUp();
+        $('header').slideUp();
+        $('#fullscreen-button').slideUp();
+        $image.css('padding-bottom', '20px');
+        setTimeout(function () {
+            $image.css('padding-bottom', '0px');
+        }, 20);
     }
 
 });
